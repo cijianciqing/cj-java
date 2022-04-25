95f2e15e31a84fcee48e8dd677eae355f09c0b39
cjcq <425430798@qq.com>
2022-04-25 11:38:42 +0800

    19. 使用AtomicStampedReference解决ABA问题

--- sgu-interview-juc/src/main/java/cj/java/juc/aba/ABADemo.java
+++ sgu-interview-juc/src/main/java/cj/java/juc/aba/ABADemo.java
@@ -40,7 +40,42 @@ public class ABADemo {
             e.printStackTrace();
         }
 
+        System.out.println("-----------------ABA问题的解决--------------------");
 
+        new Thread("t3") {
+            @Override
+            public void run() {
+                int stamp = atomicStampedReference.getStamp();
+                System.out.println(getName() + "\t第一次版本号：" + stamp);
+                try {
+                    //t3线程休眠1秒中,确保t4也拿到初始的版本号
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                boolean result3_2 = atomicStampedReference.compareAndSet(100, 101, atomicStampedReference.getStamp(), atomicStampedReference.getStamp() + 1);
+                System.out.println(getName() + "\t第二次版本号：" + atomicStampedReference.getStamp()+" result: "+result3_2);
+                boolean result3_3 = atomicStampedReference.compareAndSet(101, 100, atomicStampedReference.getStamp(), atomicStampedReference.getStamp() + 1);
+                System.out.println(getName() + "\t第三次版本号：" + atomicStampedReference.getStamp()+" result: "+result3_3);
+            }
+        }.start();
+
+        new Thread("t4") {
+            @Override
+            public void run() {
+                int stamp = atomicStampedReference.getStamp();
+                System.out.println(getName() + "\t第一次版本号：" + stamp);
+                try {
+                    //t4线程休眠3秒中,确保t3完成一次ABA操作
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                boolean result = atomicStampedReference.compareAndSet(100, 2020, stamp, stamp + 1);
+                System.out.println(getName() + "\t是否修改成功," + result + "\t当前最新实际版本号：" + atomicStampedReference.getStamp());
+                System.out.println(getName() + "\t当前实际最新值：" + atomicStampedReference.getReference());
+            }
+        }.start();
     }
 }
 
